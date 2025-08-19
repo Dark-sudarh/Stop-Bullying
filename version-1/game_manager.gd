e602d35b9edde0c0597b8fc15da0eed8f8b343e3
Dark-sudarh <mohoma95@kingshigh.school.nz>
2025-08-19 13:26:02 +1200

    version 1.0.8

--- version-1/game_manager.gd
+++ version-1/game_manager.gd
@@ -1,9 +1,10 @@
 extends Node
 
-@onready var label: Label = %Label
+@onready var  fruits_label: Label = %Label
 
 var fruits = 0
 
 func add_fruit():
 	fruits += 1
 	print(fruits)
+	fruits_label.text = "fruits:"  + str(fruits)
