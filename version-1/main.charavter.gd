2e88d4084f82edd8ebdce8cc1c034bdda10fb153
Dark-sudarh <mohoma95@kingshigh.school.nz>
2025-09-18 11:01:37 +1200

    version 1.1.3
    enemy eror

--- version-1/main.charavter.gd
+++ version-1/main.charavter.gd
@@ -5,6 +5,13 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -750.0
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 
+# get the gravity from the project settings to be synced with Rigidbody nodes
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func jump():
+	velocity.y = JUMP_VELOCITY
+
+
 func _physics_process(delta: float) -> void:
 	# Animations
 	if (velocity.x > 1 || velocity.x < -1):
